865b58ad2394e8eda3327899bf0504e925346a69
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-18 19:03:13 +0100

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
